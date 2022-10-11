8907475b6f7a2d313a38a7b41e734d188ef358a3
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2022-10-11 10:41:16 +0200

    download_models.sh

---
+++ download_models.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mkdir model_ckpt
+wget https://s3.eu-central-1.amazonaws.com/avg-projects/transfuser/models_2022.zip -c -P model_ckpt
+unzip model_ckpt/models_2022.zip -d model_ckpt
+rm model_ckpt/models_2022.zip
