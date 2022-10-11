c5194d466c9d2abaad691ad92e69856f0b010374
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2022-10-11 18:43:35 +0200

    test_run: use paths defined by environment variables

--- scripts/test_run.sh
+++ scripts/test_run.sh
@@ -6,12 +6,12 @@ export ROUTES=${ROOT}/scenario_runner/srunner/data/routes_devtest.xml
 export SCENARIOS=${ROOT}/scenario_runner/srunner/data/no_scenarios.json
 export REPETITIONS=1
 export DEBUG_CHALLENGE=1
-export CHECKPOINT_ENDPOINT=${ROOT}/results/test_run.json
+export CHECKPOINT_ENDPOINT=${RESULTS_PATH}/test_run.json
 
 # export TEAM_AGENT=${ROOT}/leaderboard/leaderboard/autoagents/human_agent.py
 # export TEAM_AGENT=${ROOT}/team_code_autopilot/autopilot.py
 export TEAM_AGENT=${ROOT}/team_code_transfuser/submission_agent.py
-export TEAM_CONFIG=${ROOT}/model_ckpt/models_2022/transfuser
+export TEAM_CONFIG=${MODELS_PATH}/models_2022/transfuser
 
 # export CHALLENGE_TRACK_CODENAME=MAP
 export CHALLENGE_TRACK_CODENAME=SENSORS
