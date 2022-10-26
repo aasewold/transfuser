f2ec9b4a328a93a22fb17a550bd76fb447ab4900
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2022-10-26 09:51:43 +0200

    docker build&run scripts

---
+++ docker/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd "$(dirname "$0")/.."
+
+USER_ID=${SUDO_UID-$(id -u)}
+GROUP_ID=${SUDO_GID-$(id -g)}
+USER_NAME="$(id -un $USER_ID)"
+IMAGE="$USER_NAME/transfuser"
+
+docker run \
+    -it \
+    --rm \
+    --network host \
+    --gpus all \
+    -e PUID=$USER_ID \
+    -e PGID=$GROUP_ID \
+    -v "$(pwd)/models":/models \
+    -v "$(pwd)/results":/results \
+    -v "$(pwd)/dataset":/dataset \
+    -v "$(pwd)":/code/transfuser \
+    "$IMAGE"
