1c4a928925fed0fb4b598b8d605795e122d300fa
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2022-11-08 12:41:37 +0100

    docker: pip cache

--- docker/build.sh
+++ docker/build.sh
@@ -6,7 +6,7 @@ USER_ID=${SUDO_UID-$(id -u)}
 USER_NAME="$(id -un $USER_ID)"
 IMAGE="$USER_NAME/transfuser"
 
-docker build \
+docker buildx build \
     -f docker/Dockerfile \
     -t "$IMAGE" \
     .
