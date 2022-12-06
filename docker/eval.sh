96a5f73da129d8f1250a9aa3a7f7f250616811b0
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2022-12-06 17:15:13 +0100

    scripts: eval --carla argument

--- docker/eval.sh
+++ docker/eval.sh
@@ -9,21 +9,32 @@ IMAGE="$USER_NAME/transfuser"
 
 TZ=${TZ:-$(cat /etc/timezone)}
 
-echo "Starting CARLA"
-CARLA_CONTAINER_ID=$(
-    docker run \
-    --detach \
-    --rm \
-    --gpus '"device=0"' \
-    --network host \
-    --user $USER_ID:$GROUP_ID \
-    carlasim/carla:0.9.13 \
-    ./CarlaUE4.sh \
-        -RenderOffScreen
-)
+if [ "$1" = "--carla" ]; then
+    RUN_CARLA=1
+    shift
+else
+    RUN_CARLA=0
+fi
 
-echo "Waiting for server to come online"
-sleep 15
+if [ "$RUN_CARLA" -eq 1 ]; then
+    echo "Starting CARLA"
+    CARLA_CONTAINER_ID=$(
+        docker run \
+        --detach \
+        --rm \
+        --gpus '"device=0"' \
+        --network host \
+        --user $USER_ID:$GROUP_ID \
+        carlasim/carla:0.9.13 \
+        ./CarlaUE4.sh \
+            -RenderOffScreen
+    )
+
+    echo "Waiting for server to come online"
+    sleep 15
+else
+    echo "Not starting CARLA"
+fi
 
 docker run \
     -it \
@@ -45,4 +56,7 @@ docker run \
         $1 \
         $2
 
-docker stop $CARLA_CONTAINER_ID
+if [ "$RUN_CARLA" -eq 1 ]; then
+    echo "Stopping CARLA"
+    docker stop "$CARLA_CONTAINER_ID"
+fi
